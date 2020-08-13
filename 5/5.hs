f9366326c097e5eab7e8a1c94c1464dc6e37d456
slava <slavaavals616@gmail.com>
2020-08-13 15:04:18 +0400

    5

---
+++ 5/5.hs
@@ -0,0 +1,11 @@
+
+getUrl domain pathname token = domain ++ "/" ++ pathname ++ "?token=" ++ token
+
+{- HLINT ignore -}
+bindBasicDomain domain = \getUrl pathname token -> getUrl domain pathname token
+
+domainBuilder = bindBasicDomain "http://example.com"
+
+getApiRequestBuilder geturl domainBuilder token = \pathname -> domainBuilder geturl pathname token
+
+getUrlByPathname = getApiRequestBuilder getUrl domainBuilder "toke1"

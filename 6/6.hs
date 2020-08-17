3ee35fccc4438b6fbf3eaf1840ac50cd3b4cb7e3
slava <slavaavals616@gmail.com>
2020-08-17 15:28:45 +0400

    6 add example

--- 6/6.hs
+++ 6/6.hs
@@ -2,3 +2,11 @@
 
 assignToGroups n aList = zip groups aList
   where groups = cycle [1..n]
+
+isFirstHalf el aList = if el `elem` firstHalf
+  then True
+  else False
+
+  where
+        middleIdx = length aList `div` 2
+        firstHalf = take middleIdx aList

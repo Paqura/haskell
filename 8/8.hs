d19e6ea9da029d4b418697d6227c5de09277b0bf
slava <slavaavals616@gmail.com>
2020-08-21 10:52:20 +0400

    123

--- 8/8.hs
+++ 8/8.hs
@@ -13,3 +13,12 @@ myRemove fn (x:xs) =
   if fn x
     then myRemove fn xs
   else x : myRemove fn xs
+
+-- разворот
+rcons xs y = y:xs
+myReverse xs = foldl rcons [] xs
+
+-- тут initVal уже будет финальным результатом
+myFoldl operand initVal [] = initVal
+myFoldl operand initVal (x:xs) = myFoldl operand result xs
+  where result = initVal (operand) x

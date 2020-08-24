ad62d537da3e2413260910765f2a56dca42ee250
Vyacheslav Popov <slavaavals616@gmail.com>
2020-08-24 20:07:09 +0400

    9

---
+++ 9/9.hs
@@ -0,0 +1,12 @@
+cup ml = \_ -> ml
+
+coffeeCup = cup 500
+
+getMl aCup = aCup (\ml -> ml)
+
+drink aCup mlDrank = cup (ml - mlDrank)
+  where ml = getMl aCup
+
+afterASip = drink coffeeCup 30
+-- чтобы проверить результат
+-- getMl afterSip

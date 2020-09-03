dc31a5109a37039095b43e2fe9af29a7044155f2
Vyacheslav Popov <slavaavals616@gmail.com>
2020-09-03 21:50:50 +0400

    update final

--- types/final-1.hs
+++ types/final-1.hs
@@ -15,3 +15,21 @@ smallestCharNumber = fromEnum (minBound :: Char)
 rotateEnChar :: Char -> Char
 rotateEnChar charToEncrypt = rotateN size charToEncrypt
   where size = 1 + largestCharNumber
+
+message :: [FourLetterAlphabet]
+message = [L1, L3, L1, L4, L2, L4]
+
+fourLetterEncoder :: [FourLetterAlphabet] -> [FourLetterAlphabet]
+fourLetterEncoder message = map rotN message
+  where size = 1 + fromEnum (maxBound :: FourLetterAlphabet)
+        rotN = rotateN size
+
+-- всё это работает rotateN только с четными алфавитами
+-- вот для нечетных
+
+rotateNImproved size l = toEnum rotation
+  where half = size `div` 2
+        offset = if even size 
+                   then fromEnum l + half
+                 else 1 + fromEnum l + half
+        rotation = offset `mod` size   

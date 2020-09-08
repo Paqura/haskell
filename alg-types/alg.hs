3027599ebed6631d10646224e4569ea70d570678
Vyacheslav Popov <slavaavals616@gmail.com>
2020-09-08 16:10:42 +0400

    alg data types step1

---
+++ alg-types/alg.hs
@@ -0,0 +1,49 @@
+type FirstName = String
+type LastName = String
+
+data Name = Name FirstName LastName
+
+data Author = Author Name
+data Artist = Person Name | Band String
+
+data Creator = AuthorCreate Author | ArtistCreate Artist
+
+data Book = Book {
+    author :: Creator
+  , isbn :: String
+  , bookTitle :: String
+  , year :: Int
+  , bookPrice :: Double
+}
+
+data VinylRecord = VinylRecord {
+    artist :: Creator
+  , recordTitle :: String
+  , recordYear :: Int
+  , recordPrice :: Double
+}
+
+data CollectibleToy = CollectibleToy {
+    name :: String
+  , description :: String
+  , toyPrice :: Double
+}
+
+data StoreItem =
+  BookItem Book | RecordItem VinylRecord | ToyItem CollectibleToy
+
+price :: StoreItem -> Double
+
+price (BookItem book) = bookPrice book
+price (RecordItem record) = recordPrice record
+price (ToyItem toy) = toyPrice toy
+
+-- пример использования
+
+bookAuthor :: Creator
+bookAuthor = AuthorCreate (Author (Name "Slava" "Avals"))
+
+book :: StoreItem
+book = BookItem (Book bookAuthor "12312" "book title" 2020 1200)
+
+-- price book -> 1200.0

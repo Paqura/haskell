2186d5d5684daf8ddb4ad1c96c868d740dbade37
slava <slavaavals616@gmail.com>
2020-11-09 19:39:39 +0400

    update text.hs

--- text.hs
+++ text.hs
@@ -18,6 +18,18 @@ import qualified Data.Text.IO as TIO
 -- combinedTextSemigroup :: T.Text
 -- combinedTextSemigroup = "some" <> " " <> "text"
 
+-- word with io
+-- helloPerson :: T.Text -> T.Text
+-- helloPerson name = T.intercalate " " ["Hello", name]
+
+-- main :: IO()
+-- main = do
+--   TIO.putStrLn "Hello! What are you name?"
+--   name <- TIO.getLine
+--   let statement = helloPerson name
+--   TIO.putStrLn statement
+
+-- text hightlighter
 dharma :: T.Text
 dharma = "hell"
 

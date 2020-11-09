fb59c3c79bffd806e9cd35d9cd4699c83e6e01df
slava <slavaavals616@gmail.com>
2020-11-09 19:09:00 +0400

    text

---
+++ text.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+import qualified Data.Text as T
+
+aWord :: T.Text
+aWord = "Work with with"
+
+-- main :: IO()
+-- main = do
+--   print $ T.splitOn " " aWord
+-- ["Work", "with", "with"]
+
+-- что-то типо `join`
+-- main :: IO()
+-- main = do
+--   print $ T.intercalate " " ["Work", "with", "with"]
+

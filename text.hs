e6e06414fdf5fadb7674d65a14467c64628ac938
slava <slavaavals616@gmail.com>
2020-11-09 19:30:50 +0400

    text - hightlight

--- text.hs
+++ text.hs
@@ -1,16 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
 import qualified Data.Text as T
-
-aWord :: T.Text
-aWord = "Work with with"
+import qualified Data.Text.IO as TIO
 
 -- main :: IO()
 -- main = do
---   print $ T.splitOn " " aWord
+--   print $ T.splitOn " " "Work with with"
 -- ["Work", "with", "with"]
 
 -- что-то типо `join`
 -- main :: IO()
 -- main = do
 --   print $ T.intercalate " " ["Work", "with", "with"]
+
+-- combinedTextMonoid :: T.Text
+-- combinedTextMonoid = mconcat ["some", " ", "text"]
+
+-- combinedTextSemigroup :: T.Text
+-- combinedTextSemigroup = "some" <> " " <> "text"
+
+dharma :: T.Text
+dharma = "hell"
+
+bgText :: T.Text
+bgText = "hell green color brahell hell few times"
+
+highlight :: T.Text -> T.Text -> T.Text
+highlight query fullText = T.intercalate highlighted pieces
+  where
+    pieces = T.splitOn query fullText
+    highlighted = mconcat ["{", query, "}"]
+
+main :: IO()
+main = do
+  TIO.putStrLn $ highlight dharma bgText
+
 

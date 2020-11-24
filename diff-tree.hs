cc9acc0df7c1a46a9bfc19673537e691e3b1ed20
slava <slavaavals616@gmail.com>
2020-11-24 20:07:15 +0400

    add super simple realization of diff

--- diff-tree.hs
+++ diff-tree.hs
@@ -15,11 +15,13 @@ type Props = Map.Map String String
 
 createProps :: [(String, String)] -> Props
 createProps = Map.fromList
+
+type Children = [Node]
 data Node = Node
   {
       nodeType :: NodeType
     , props :: Props
-    , children :: [Node]
+    , children :: Children
   }
 
 printNode :: Node -> String
@@ -35,12 +37,35 @@ printNode (Node nodeType props children) =
 instance Show Node where
   show (Node nodeType props children) = printNode (Node nodeType props children)
 
-createNode :: NodeType -> Props -> [Node] -> Node
-createNode nodeType props children = Node nodeType props allChild
+createNode :: NodeType -> Props -> Children -> Node
+createNode nodeType props children = Node nodeType props allChildren
   where
-    isLastChild = null children
+    childFree = null children
     Node cType cProps cChildren = head children
-    allChild
-          | isLastChild = []
+    allChildren
+          | childFree = []
           | otherwise = [createNode cType cProps cChildren]
 
+-- example
+-- node1 = createNode TNode (Map.fromList [("class", "test")]) []
+-- {
+--  nodeType:Node
+--  props:fromList [("class","test")]
+--  children:[]
+-- }
+
+-- node2 = createNode TText (Map.fromList [("class", "test"), ("newProp", "woo")]) [node1]
+
+type NodePatch = Node
+
+vDiff :: Node -> Node -> NodePatch
+vDiff (Node n1type n1props n1children) (Node n2type n2props n2children) = patch
+  where
+      isSameType = n1type == n2type
+      isSamePropsLen = Map.size n1props == Map.size n2props
+      isSameChildren = length n1children == length n2children
+      patch
+        | not isSameType = vDiff (Node n2type n1props n1children) (Node n2type n2props n2children)
+        | not isSamePropsLen = vDiff (Node n1type n2props n1children) (Node n2type n2props n2children)
+        | not isSameChildren = vDiff (Node n1type n1props n2children) (Node n2type n2props n2children)
+        | otherwise = Node n1type n1props n1children
